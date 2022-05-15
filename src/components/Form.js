f26627c3995ad81c4c22a21d081f5c85a4ca1785
unknown <ronny.garcia@epn.edu.ec>
2022-05-15 08:07:44 -0500

    adding validations to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,18 @@ import "./Form.css"
 const Form = () => {
   return (
     <div className='form__container'>
-      <form>
+      <form className='needs-validation'>
         <div className="form__head">
           <h1 className="form__title">Bienvenid/a</h1>
         </div>
         <div className="form__body">
           <div className="form__field">
             <label htmlFor="user">Usuario</label>
-            <input type="email" placeholder='example@example.com' name='user' className="user" />
+            <input type="email" placeholder='example@example.com' name='user' className="user" required/>
           </div>
           <div className="form__field">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" placeholder='***********' name='password' className="form__password" />
+            <input type="password" placeholder='***********' name='password' className="form__password" required />
           </div>
           <div className="button__container">
             <button className='button__login'>Ingresar</button>
